36e3a08fcc176df893ccfeec130c1e0d20e75fbb
Matt Guthaus <mrg@ucsc.edu>
2016-11-10 09:35:06 -0800

    Override spice model dir by environment variable in FREEPDK as well

--- technology/setup_scripts/setup_openram_freepdk45.py
+++ technology/setup_scripts/setup_openram_freepdk45.py
@@ -24,7 +24,10 @@ os.environ["MGC_TMPDIR"] = "/tmp"
 
 DRCLVS_HOME= PDK_DIR+"/ncsu_basekit/techfile/calibre"
 os.environ["DRCLVS_HOME"] = DRCLVS_HOME 
-os.environ["SPICE_MODEL_DIR"] = PDK_DIR+"/ncsu_basekit/models/hspice/tran_models/models_nom"
+try:
+    SPICE_MODEL_DIR = os.path.abspath(os.environ.get("SPICE_MODEL_DIR"))
+except:
+    os.environ["SPICE_MODEL_DIR"] = PDK_DIR+"/ncsu_basekit/models/hspice/tran_models/models_nom"
 
 ##########################
 #Paths required for OPENRAM to function

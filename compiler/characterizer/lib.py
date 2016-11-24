841532a52fcc328d9559f281b70b6d8792c188df
Matt Guthaus <mrg@ucsc.edu>
2016-11-23 17:18:48 -0800

    Change characterizer to be one data structure. Add approximate diff for lib file.

--- compiler/characterizer/lib.py
+++ compiler/characterizer/lib.py
@@ -61,12 +61,14 @@ class lib:
         probe_address = "1" * self.addr_size
         probe_data = self.word_size - 1
 
-        data , power = self.d.analyze(probe_address, probe_data)
+        data = self.d.analyze(probe_address, probe_data)
         for i in data.keys():
+            if i == "read_power" or i == "write_power":
+                continue
             data[i] = ch.round_time(data[i])
 
       
-        self.write_data_bus(data, power, times)
+        self.write_data_bus(data, times)
         self.write_addr_bus(times)
         self.write_control_pins(times)
         self.write_clk(data)
@@ -199,7 +201,7 @@ class lib:
 
 
 
-    def write_data_bus(self, data, power, times):
+    def write_data_bus(self, data, times):
         """ Adds data bus timing results."""
         self.lib.write("    bus(DATA){\n")
         self.lib.write("        bus_type  : DATA; \n")
@@ -217,10 +219,10 @@ class lib:
 	self.lib.write("        internal_power(){\n")
 	self.lib.write("            when : \"OEb & !clk\"; \n")
 	self.lib.write("            rise_power(INPUT_BY_TRANS_FOR_SIGNAL){\n")
-	self.lib.write("                values(\"{0}\");\n".format(power["Write_Power"]* 1e3))
+	self.lib.write("                values(\"{0}\");\n".format(data["write_power"]* 1e3))
 	self.lib.write("            }\n")
 	self.lib.write("            fall_power(INPUT_BY_TRANS_FOR_SIGNAL){\n")
-	self.lib.write("                values(\"{0}\");\n".format(power["Write_Power"]* 1e3))
+	self.lib.write("                values(\"{0}\");\n".format(data["write_power"]* 1e3))
 	self.lib.write("            }\n")
         self.lib.write("        }\n")
 	self.write_timing(times)
@@ -231,10 +233,10 @@ class lib:
 	self.lib.write("        internal_power(){\n")
 	self.lib.write("            when : \"!OEb & !clk\"; \n")
 	self.lib.write("            rise_power(INPUT_BY_TRANS_FOR_SIGNAL){\n")
-	self.lib.write("                values(\"{0}\");\n".format(power["Read_Power"]* 1e3))
+	self.lib.write("                values(\"{0}\");\n".format(data["read_power"]* 1e3))
 	self.lib.write("            }\n")
 	self.lib.write("            fall_power(INPUT_BY_TRANS_FOR_SIGNAL){\n")
-	self.lib.write("                values(\"{0}\");\n".format(power["Read_Power"]* 1e3))
+	self.lib.write("                values(\"{0}\");\n".format(data["read_power"]* 1e3))
 	self.lib.write("            }\n")
         self.lib.write("        }\n")
 	self.lib.write("        timing(){ \n")
--- compiler/tests/testutils.py
+++ compiler/tests/testutils.py
@@ -1,5 +1,4 @@
 
-
 def isclose(value1,value2,error_tolerance=1e-2):
     """ This is used to compare relative values. """
     import debug
@@ -10,6 +9,79 @@ def isclose(value1,value2,error_tolerance=1e-2):
     else:
         debug.info(2,"CLOSE {0} {1} relative diff={2}".format(value1,value2,relative_diff))
     return (check)
+
+def relative_compare(value1,value2,error_tolerance):
+    """ This is used to compare relative values. """
+    if (value1==value2): # if we don't need a relative comparison!
+        return True
+    return (abs(value1 - value2) / max(value1,value2) <= error_tolerance)
+
+def isapproxdiff(f1, f2, error_tolerance=0.001):
+    """Compare two files.
+
+    Arguments:
+
+    f1 -- First file name
+
+    f2 -- Second file name
+
+    Return value:
+
+    True if the files are the same, False otherwise.
+
+    """
+    import re
+    import debug
+
+    with open(f1, 'rb') as fp1, open(f2, 'rb') as fp2:
+        while True:
+            b1 = fp1.readline()
+            b2 = fp2.readline()
+            #print "b1:",b1,
+            #print "b2:",b2,
+
+            # 1. Find all of the floats using a regex
+            numeric_const_pattern = r"""
+            [-+]? # optional sign
+            (?:
+            (?: \d* \. \d+ ) # .1 .12 .123 etc 9.1 etc 98.1 etc
+            |
+            (?: \d+ \.? ) # 1. 12. 123. etc 1 12 123 etc
+            )
+            # followed by optional exponent part if desired
+            (?: [Ee] [+-]? \d+ ) ?
+            """
+            rx = re.compile(numeric_const_pattern, re.VERBOSE)
+            b1_floats=rx.findall(b1)
+            b2_floats=rx.findall(b2)
+            debug.info(3,"b1_floats: "+str(b1_floats))
+            debug.info(3,"b2_floats: "+str(b2_floats))
+        
+            # 2. Remove the floats from the string
+            for f in b1_floats:
+                b1=b1.replace(str(f),"")
+            for f in b2_floats:
+                b2=b2.replace(str(f),"")
+            #print "b1:",b1,
+            #print "b2:",b2,
+            
+            # 3. Check if remaining string matches
+            if b1 != b2:
+                debug.info(2,"Line: {0}\n!=\nLine: {1}".format(b1,b2))
+                return False
+
+        # 4. Now compare that the floats match
+            if len(b1_floats)!=len(b2_floats):
+                debug.info(2,"Len {0} != {1}".format(len(b1_floats),len(b2_floats)))
+                return False
+            for (f1,f2) in zip(b1_floats,b2_floats):
+                if not relative_compare(float(f1),float(f2),error_tolerance):
+                    debug.info(2, "Float {0} != {1}".format(f1,f2))
+                    return False
+
+            if not b1:
+                return True
+
 
 def isdiff(file1,file2):
     """ This is used to compare two files and display the diff if they are different.. """

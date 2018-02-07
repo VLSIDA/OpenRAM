ed194ad47b520362184250ddbdadbb1192050041
Matt Guthaus <mrg@ucsc.edu>
2018-02-07 10:05:21 -0800

    Remove spice dir env variable for freepdk.

--- technology/setup_scripts/setup_openram_freepdk45.py
+++ technology/setup_scripts/setup_openram_freepdk45.py
@@ -28,10 +28,11 @@ except:
     DRCLVS_HOME= PDK_DIR+"/ncsu_basekit/techfile/calibre"
 os.environ["DRCLVS_HOME"] = DRCLVS_HOME
 
-try:
-     SPICE_MODEL_DIR = os.path.abspath(os.environ.get("SPICE_MODEL_DIR"))
-except:
-    os.environ["SPICE_MODEL_DIR"] = PDK_DIR+"/ncsu_basekit/models/hspice/tran_models/models_nom"
+# try:
+#      SPICE_MODEL_DIR = os.path.abspath(os.environ.get("SPICE_MODEL_DIR"))
+# except:
+# Always use the one in the PDK dir for FreePDK45
+os.environ["SPICE_MODEL_DIR"] = PDK_DIR+"/ncsu_basekit/models/hspice/tran_models/models_nom"
 
 ##########################
 #Paths required for OPENRAM to function

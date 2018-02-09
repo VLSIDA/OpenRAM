7c83ef3f04ed88379f6bd3cdd8f67394fd225697
Matt Guthaus <mrg@ucsc.edu>
2018-02-09 10:27:43 -0800

    Fix missing subdir name. Comment about organization.

--- compiler/globals.py
+++ compiler/globals.py
@@ -219,8 +219,9 @@ def setup_paths():
         debug.error("$OPENRAM_HOME is not properly defined.",1)
     debug.check(os.path.isdir(OPENRAM_HOME),"$OPENRAM_HOME does not exist: {0}".format(OPENRAM_HOME))
 
-    # Add all of the submodules to the python path
-    for subdir in ["gdsMill", "tests", "router", "modules", "layout", "pgates"]:
+    # Add all of the subdirs to the python path
+    # These subdirs are modules and don't need to be added: characterizer, verify
+    for subdir in ["gdsMill", "tests", "router", "modules", "base", "pgates"]:
         full_path = "{0}/{1}".format(OPENRAM_HOME,subdir)
         debug.check(os.path.isdir(full_path),
                     "$OPENRAM_HOME/{0} does not exist: {1}".format(subdir,full_path))

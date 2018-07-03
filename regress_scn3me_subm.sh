ded87dbbe74e0ffcaa8750d0746a129570391069
Matt Guthaus <mrg@ucsc.edu>
2018-07-03 11:49:29 -0700

    Add regress runner scripts

---
+++ regress_scn3me_subm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is a bash utility script to set the paths to the current
+# directory of OpenRAM. It must be sourced in the local directory
+# like this:
+# source setpaths.sh
+
+export OPENRAM_HOME="`pwd`/compiler"
+export OPENRAM_TECH="`pwd`/technology"
+python3 $OPENRAM_HOME/tests/regress.py -t scnn3me_subm

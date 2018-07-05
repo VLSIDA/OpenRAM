8ad503d0212efef6b27691b529826b8cc79fc45d
Matt Guthaus <mrg@ucsc.edu>
2018-07-05 14:31:31 -0700

    Remove regress scripts from repo.

--- regress_freepdk45.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This is a bash utility script to set the paths to the current
-# directory of OpenRAM. It must be sourced in the local directory
-# like this:
-# source setpaths.sh
-
-export OPENRAM_HOME="`pwd`/compiler"
-export OPENRAM_TECH="`pwd`/technology"
-python3 $OPENRAM_HOME/tests/regress.py -t freepdk45

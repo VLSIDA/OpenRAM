bb79d9a62de3b1ebc96bb2a429b57e42ef4ee5b3
Hunter Nichols <hznichol@ucsc.edu>
2018-09-24 18:38:15 -0700

    Added regex pattern matching to trim_spice to handle multiport.

--- compiler/example_config_scn4m_subm.py
+++ compiler/example_config_scn4m_subm.py
@@ -8,3 +8,8 @@ temperatures = [ 25 ]
 
 output_path = "temp"
 output_name = "sram_{0}_{1}_{2}_{3}".format(word_size,num_words,num_banks,tech_name)
+
+#Setting for multiport
+# netlist_only = True
+# bitcell = "pbitcell"
+# replica_bitcell="replica_pbitcell"

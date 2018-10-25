b1f3bd97e5c49129474bd672b1d5899d580135e7
Matt Guthaus <mrg@ucsc.edu>
2018-10-24 17:01:00 -0700

    Enable all the 1bank tests. Mostly work in SCMOS.

--- compiler/tests/20_sram_1bank_4mux_test.py
+++ compiler/tests/20_sram_1bank_4mux_test.py
@@ -11,7 +11,7 @@ import globals
 from globals import OPTS
 import debug
 
-@unittest.skip("SKIPPING 20_sram_1bank_2mux_test")
+#@unittest.skip("SKIPPING 20_sram_1bank_4mux_test")
 class sram_1bank_4mux_test(openram_test):
 
     def runTest(self):

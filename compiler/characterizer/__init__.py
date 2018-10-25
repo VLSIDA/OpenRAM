57fb847d50bdff2d8738064fe56fb30dadc47043
Matt Guthaus <mrg+openram@ucsc.edu>
2018-10-25 09:08:56 -0700

    Fix check for missing simulator type in characterizer

--- compiler/characterizer/__init__.py
+++ compiler/characterizer/__init__.py
@@ -18,7 +18,7 @@ if not OPTS.analytical_delay:
 
     if OPTS.spice_name != "":
         OPTS.spice_exe=find_exe(OPTS.spice_name)
-        if OPTS.spice_exe=="":
+        if OPTS.spice_exe=="" or OPTS.spice_exe==None:
             debug.error("{0} not found. Unable to perform characterization.".format(OPTS.spice_name),1)
     else:
         (OPTS.spice_name,OPTS.spice_exe) = get_tool("spice",["xa", "hspice", "ngspice", "ngspice.exe"])

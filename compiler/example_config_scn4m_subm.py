642dc8517c5809ad2b46ec371f7e63899d50e22b
Hunter Nichols <hznichol@ucsc.edu>
2018-11-01 14:05:55 -0700

    Added no mux functional test for 1rw+1r. Delay characterization also works for the custom cell as well.

--- compiler/example_config_scn4m_subm.py
+++ compiler/example_config_scn4m_subm.py
@@ -10,9 +10,15 @@ output_path = "temp"
 output_name = "sram_{0}_{1}_{2}".format(word_size,num_words,tech_name)
 
 #Setting for multiport
-netlist_only = True
-bitcell = "pbitcell"
-replica_bitcell="replica_pbitcell"
-num_rw_ports = 1
-num_r_ports = 1
-num_w_ports = 1
+# netlist_only = True
+# num_rw_ports = 1
+# num_r_ports = 1
+# num_w_ports = 0
+
+#Pbitcell modules for multiport
+#bitcell = "pbitcell"
+#replica_bitcell="replica_pbitcell"
+
+#Custom 1rw+1r multiport cell. Set the above port numbers to rw = 1, r = 1, w = 0
+# bitcell = "bitcell_1rw_1r"
+# replica_bitcell = "replica_bitcell_1rw_1r"

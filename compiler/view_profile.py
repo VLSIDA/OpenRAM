82a09be026be5382bac6fc4b8d027be4f75750df
Matt Guthaus <mrg@ucsc.edu>
2019-01-30 08:42:25 -0800

    Move inspect into if statement for runtime

--- compiler/view_profile.py
+++ compiler/view_profile.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 import pstats
-p = pstats.Stats(‘profile.dat’)
+p = pstats.Stats("profile.dat")
 p.strip_dirs()
-p.sort_stats(‘cumulative’)
-p.print_stats(50)
+p.sort_stats("cumulative")
+#p.print_stats(50)
+p.print_stats()
 

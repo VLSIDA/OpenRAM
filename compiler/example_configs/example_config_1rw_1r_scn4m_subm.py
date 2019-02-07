bfc20a9aa9958308adc431bc399046639a440164
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2019-02-07 06:38:07 -0800

    removes debug corners

--- compiler/example_configs/example_config_1rw_1r_scn4m_subm.py
+++ compiler/example_configs/example_config_1rw_1r_scn4m_subm.py
@@ -10,7 +10,7 @@ num_w_ports = 0
 tech_name = "scn4m_subm"
 process_corners = ["TT"]
 supply_voltages = [5.0]
-temperatures = [25,50]
+temperatures = [25]
 
 output_path = "temp"
 output_name = "sram_1rw_1r_{0}_{1}_{2}".format(word_size,num_words,tech_name)

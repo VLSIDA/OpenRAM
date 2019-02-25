a4b53683025f508d04e4db34490c6cd7aafa1467
Matt Guthaus <mrg@ucsc.edu>
2019-02-25 14:57:18 -0800

    Add total size in warning for output size.

--- compiler/globals.py
+++ compiler/globals.py
@@ -419,8 +419,9 @@ def report_status():
     debug.print_raw("Technology: {0}".format(OPTS.tech_name))
     total_size = OPTS.word_size*OPTS.num_words*OPTS.num_banks
     debug.print_raw("Total size: {} bits".format(total_size))
-    if total_size>=2**15:
-        debug.warning("Requesting such a large memory size ({0}) will have a large run-time. Consider using multiple smaller banks.")
+    if total_size>=2**14:
+        debug.warning("Requesting such a large memory size ({0}) will have a large run-time. ".format(total_size) +
+                      "Consider using multiple smaller banks.")
     debug.print_raw("Word size: {0}\nWords: {1}\nBanks: {2}".format(OPTS.word_size,
                                                           OPTS.num_words,
                                                           OPTS.num_banks))

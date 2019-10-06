67c768d22c761488c7338b01de6867b2fd796815
vagrant <vagrant@ubuntu1804.localdomain>
2019-10-06 01:08:23 +0000

    Refactor bitcell to bitcell_base. Pep8 format bitcells.

--- compiler/bitcells/bitcell_1w_1r.py
+++ compiler/bitcells/bitcell_1w_1r.py
@@ -5,13 +5,13 @@
 # (acting for and on behalf of Oklahoma State University)
 # All rights reserved.
 #
-import design
 import debug
 import utils
-from tech import GDS,layer,parameter,drc
-import logical_effort
+from tech import GDS, layer
+import bitcell_base
 
-class bitcell_1w_1r(design.design):
+
+class bitcell_1w_1r(bitcell_base.bitcell_base):
     """
     A single bit cell (6T, 8T, etc.)  This module implements the
     single memory cell used in the design. It is a hand-made cell, so
@@ -20,14 +20,17 @@ class bitcell_1w_1r(design.design):
     """
 
     pin_names = ["bl0", "br0", "bl1", "br1", "wl0", "wl1", "vdd", "gnd"]
-    type_list = ["OUTPUT", "OUTPUT", "INPUT", "INPUT", "INPUT", "INPUT", "POWER", "GROUND"]
-    storage_nets = ['Q', 'Q_bar']    
-    (width,height) = utils.get_libcell_size("cell_1w_1r", GDS["unit"], layer["boundary"])
+    type_list = ["OUTPUT", "OUTPUT", "INPUT", "INPUT",
+                 "INPUT", "INPUT", "POWER", "GROUND"]
+    storage_nets = ['Q', 'Q_bar']
+    (width, height) = utils.get_libcell_size("cell_1w_1r",
+                                             GDS["unit"],
+                                             layer["boundary"])
     pin_map = utils.get_libcell_pins(pin_names, "cell_1w_1r", GDS["unit"])
 
     def __init__(self, name=""):
         # Ignore the name argument
-        design.design.__init__(self, "cell_1w_1r")
+        bitcell_base.bitcell_base.__init__(self, "cell_1w_1r")
         debug.info(2, "Create bitcell with 1W and 1R Port")
 
         self.width = bitcell_1w_1r.width
@@ -36,15 +39,11 @@ class bitcell_1w_1r(design.design):
         self.add_pin_types(self.type_list)
         self.nets_match = self.do_nets_exist(self.storage_nets)
 
-    def get_stage_effort(self, load):
-        parasitic_delay = 1
-        size = 0.5 #This accounts for bitline being drained thought the access TX and internal node
-        cin = 3 #Assumes always a minimum sizes inverter. Could be specified in the tech.py file.
-        read_port_load = 0.5 #min size NMOS gate load
-        return logical_effort.logical_effort('bitline', size, cin, load+read_port_load, parasitic_delay, False)
-   
     def get_bitcell_pins(self, col, row):
-        """ Creates a list of connections in the bitcell, indexed by column and row, for instance use in bitcell_array """
+        """
+        Creates a list of connections in the bitcell,
+        indexed by column and row, for instance use in bitcell_array
+        """
         bitcell_pins = ["bl0_{0}".format(col),
                         "br0_{0}".format(col),
                         "bl1_{0}".format(col),
@@ -57,7 +56,7 @@ class bitcell_1w_1r(design.design):
     
     def get_all_wl_names(self):
         """ Creates a list of all wordline pin names """
-        row_pins = ["wl0", "wl1"]    
+        row_pins = ["wl0", "wl1"]
         return row_pins
     
     def get_all_bitline_names(self):
@@ -105,40 +104,15 @@ class bitcell_1w_1r(design.design):
     
     def get_wl_name(self, port=0):
         """Get wl name by port"""
-        debug.check(port<2,"Two ports for bitcell_1rw_1r only.")
+        debug.check(port < 2, "Two ports for bitcell_1rw_1r only.")
         return "wl{}".format(port)
     
-    def analytical_power(self, corner, load):
-        """Bitcell power in nW. Only characterizes leakage."""
-        from tech import spice
-        leakage = spice["bitcell_leakage"]
-        dynamic = 0 #temporary
-        total_power = self.return_power(dynamic, leakage)
-        return total_power
-
-    def input_load(self):
-        """Return the relative capacitance of the access transistor gates"""
-        
-        # FIXME: This applies to bitline capacitances as well.
-        # FIXME: sizing is not accurate with the handmade cell. Change once cell widths are fixed.
-        access_tx_cin = parameter["6T_access_size"]/drc["minwidth_tx"]
-        return 2*access_tx_cin
-        
-    def get_storage_net_names(self):
-        """Returns names of storage nodes in bitcell in  [non-inverting, inverting] format."""
-        #Checks that they do exist
-        if self.nets_match:
-            return self.storage_nets
-        else:
-            debug.info(1,"Storage nodes={} not found in spice file.".format(self.storage_nets))
-            return None    
-        
-    def build_graph(self, graph, inst_name, port_nets):        
+    def build_graph(self, graph, inst_name, port_nets):
         """Adds edges to graph. Multiport bitcell timing graph is too complex
            to use the add_graph_edges function."""
-        pin_dict = {pin:port for pin,port in zip(self.pins, port_nets)} 
-        #Edges hardcoded here. Essentially wl->bl/br for both ports.
+        pin_dict = {pin: port for pin, port in zip(self.pins, port_nets)}
+        # Edges hardcoded here. Essentially wl->bl/br for both ports.
         # Port 0 edges
-        graph.add_edge(pin_dict["wl1"], pin_dict["bl1"], self)   
-        graph.add_edge(pin_dict["wl1"], pin_dict["br1"], self)     
-        # Port 1 is a write port, so its timing is not considered here.      
+        graph.add_edge(pin_dict["wl1"], pin_dict["bl1"], self)
+        graph.add_edge(pin_dict["wl1"], pin_dict["br1"], self)
+        # Port 1 is a write port, so its timing is not considered here.
--- compiler/bitcells/dummy_bitcell_1rw_1r.py
+++ compiler/bitcells/dummy_bitcell_1rw_1r.py
@@ -5,12 +5,13 @@
 # (acting for and on behalf of Oklahoma State University)
 # All rights reserved.
 #
-import design
 import debug
 import utils
-from tech import GDS,layer,drc,parameter
+from tech import GDS, layer
+import bitcell_base
 
-class dummy_bitcell_1rw_1r(design.design):
+
+class dummy_bitcell_1rw_1r(bitcell_base.bitcell_base):
     """
     A single bit cell which is forced to store a 0.
     This module implements the single memory cell used in the design. It
@@ -18,13 +19,18 @@ class dummy_bitcell_1rw_1r(design.design):
     the technology library. """
 
     pin_names = ["bl0", "br0", "bl1", "br1", "wl0", "wl1", "vdd", "gnd"]
-    type_list = ["OUTPUT", "OUTPUT", "OUTPUT", "OUTPUT", "INPUT", "INPUT", "POWER", "GROUND"]  
-    (width,height) = utils.get_libcell_size("dummy_cell_1rw_1r", GDS["unit"], layer["boundary"])
-    pin_map = utils.get_libcell_pins(pin_names, "dummy_cell_1rw_1r", GDS["unit"])
+    type_list = ["OUTPUT", "OUTPUT", "OUTPUT", "OUTPUT",
+                 "INPUT", "INPUT", "POWER", "GROUND"]
+    (width, height) = utils.get_libcell_size("dummy_cell_1rw_1r",
+                                             GDS["unit"],
+                                             layer["boundary"])
+    pin_map = utils.get_libcell_pins(pin_names,
+                                     "dummy_cell_1rw_1r",
+                                     GDS["unit"])
 
     def __init__(self, name=""):
-        # Ignore the name argument        
-        design.design.__init__(self, "dummy_cell_1rw_1r")
+        # Ignore the name argument
+        bitcell_base.bitcell_base.__init__(self, "dummy_cell_1rw_1r")
         debug.info(2, "Create dummy bitcell 1rw+1r object")
 
         self.width = dummy_bitcell_1rw_1r.width
@@ -32,14 +38,3 @@ class dummy_bitcell_1rw_1r(design.design):
         self.pin_map = dummy_bitcell_1rw_1r.pin_map
         self.add_pin_types(self.type_list)
 
-    def get_wl_cin(self):
-        """Return the relative capacitance of the access transistor gates"""
-        #This is a handmade cell so the value must be entered in the tech.py file or estimated.
-        #Calculated in the tech file by summing the widths of all the related gates and dividing by the minimum width.
-        #FIXME: sizing is not accurate with the handmade cell. Change once cell widths are fixed.
-        access_tx_cin = parameter["6T_access_size"]/drc["minwidth_tx"]
-        return 2*access_tx_cin
-
-    def build_graph(self, graph, inst_name, port_nets):        
-        """Dummy bitcells are cannot form a path and be part of the timing graph"""
-        return 

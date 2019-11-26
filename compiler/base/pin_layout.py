909321326d47de73d31cc95611b48c45c6566016
Matt Guthaus <mrg@ucsc.edu>
2019-11-26 13:21:29 -0800

    Ignore unused layers

--- compiler/base/pin_layout.py
+++ compiler/base/pin_layout.py
@@ -37,6 +37,8 @@ class pin_layout:
         # else it is required to be a lpp
         else:
             for (layer_name, lpp) in layer.items():
+                if not lpp:
+                    continue
                 if self.same_lpp(layer_name_pp, lpp):
                     self.layer = layer_name
                     break

2a912dab7a3debf2cc4666430e11b68057003dbd
Matt Guthaus <mrg@ucsc.edu>
2019-11-29 12:35:35 -0800

    Remove unused config files

--- technology/freepdk45/config.py
+++
@@ -1,7 +0,0 @@
-process_corners = ["TT"]
-supply_voltages = [1.0]
-temperatures = [25]
-
-drc_name = "calibre"
-lvs_name = "calibre"
-pex_name = "calibre"
--- technology/scn4m_subm/config.py
+++
@@ -1,7 +0,0 @@
-process_corners = ["TT"]
-supply_voltages = [5.0]
-temperatures = [25]
-
-drc_name = "magic"
-lvs_name = "netgen"
-pex_name = "magic"

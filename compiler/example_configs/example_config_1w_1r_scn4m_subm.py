be4893839bd8bd3ca987595253c85b652c7a2ed0
Matt Guthaus <mrg@ucsc.edu>
2019-12-16 10:05:52 -0800

    Remove old drc/lvs override name

--- compiler/example_configs/example_config_1w_1r_scn4m_subm.py
+++ compiler/example_configs/example_config_1w_1r_scn4m_subm.py
@@ -19,6 +19,3 @@ output_name = "sram_1w_1r_{0}_{1}_{2}".format(word_size,
                                               num_words,
                                               tech_name)
 
-drc_name = "magic"
-lvs_name = "netgen"
-pex_name = "magic"

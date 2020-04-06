a12b5d9e6c2e8fa723a2b8ba93c3ebd780caa5ed
mrg <mrg@ucsc.edu>
2020-04-06 13:31:31 -0700

    Split decoder pbitcell tests

--- compiler/tests/06_hierarchical_predecode3x8_pbitcell_test.py
+++ compiler/tests/06_hierarchical_predecode3x8_pbitcell_test.py
@@ -15,19 +15,24 @@ from globals import OPTS
 from sram_factory import factory
 import debug
 
-class hierarchical_predecode2x4_test(openram_test):
+class hierarchical_predecode3x8_test(openram_test):
 
     def runTest(self):
         config_file = "{}/tests/configs/config".format(os.getenv("OPENRAM_HOME"))
         globals.init_openram(config_file)
 
-        # checking hierarchical precode 2x4 for single port
-        debug.info(1, "Testing sample for hierarchy_predecode2x4")
-        a = factory.create(module_type="hierarchical_predecode2x4")
+        # checking hierarchical precode 3x8 for multi-port
+        OPTS.bitcell = "pbitcell"
+        OPTS.num_rw_ports = 1
+        OPTS.num_w_ports = 0
+        OPTS.num_r_ports = 0
+        
+        debug.info(1, "Testing sample for hierarchy_predecode3x8 (multi-port case)")
+        a = factory.create(module_type="hierarchical_predecode3x8")
         self.local_check(a)
 
         globals.end_openram()
-        
+
 # run the test from the command line
 if __name__ == "__main__":
     (OPTS, args) = globals.parse_args()

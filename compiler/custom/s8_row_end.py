5246d0a93ba845a4a7f3db5fe89de5d61eaf807d
jcirimel <jcirimel@ucsc.edu>
2020-10-05 12:10:44 -0700

    track s8 customs modules

---
+++ compiler/custom/s8_row_end.py
@@ -0,0 +1,34 @@
+# See LICENSE for licensing information.
+#
+# Copyright (c) 2016-2019 Regents of the University of California and The Board
+# of Regents for the Oklahoma Agricultural and Mechanical College
+# (acting for and on behalf of Oklahoma State University)
+# All rights reserved.
+#
+
+import debug
+import design
+import utils
+from globals import OPTS
+from tech import parameter, drc, layer, GDS
+
+class s8_row_end(design.design):
+
+
+    def __init__(self, version, name=""):
+        super().__init__(name)
+        pin_names = []
+        type_list = []
+
+        if version == "rowend":
+            self.name = "s8sram16x16_rowend"
+        elif version == "rowenda":
+            self.name = "s8sram16x16_rowenda"
+        else:
+            debug.error("Invalid type for row_end", -1)
+        design.design.__init__(self, name=self.name)
+        (self.width, self.height) = utils.get_libcell_size(self.name,
+                                        GDS["unit"],
+                                        layer["mem"],
+                                        "s8sram16x16_rowend_ce\x00")
+        pin_map = utils.get_libcell_pins(pin_names, self.name, GDS["unit"])

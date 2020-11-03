29f4ee492b9542898b4d5f6d645d40703c32bfcb
mrg <mrg@ucsc.edu>
2020-11-03 15:24:44 -0800

    Fix missing imports in replica bitcells.

--- compiler/bitcells/replica_bitcell_1w_1r.py
+++ compiler/bitcells/replica_bitcell_1w_1r.py
@@ -8,6 +8,8 @@
 import debug
 import bitcell_base
 from tech import cell_properties as props
+from tech import parameter, drc
+import logical_effort
 
 
 class replica_bitcell_1w_1r(bitcell_base.bitcell_base):
@@ -33,26 +35,26 @@ class replica_bitcell_1w_1r(bitcell_base.bitcell_base):
 
     def get_stage_effort(self, load):
         parasitic_delay = 1
-        size = 0.5 #This accounts for bitline being drained thought the access TX and internal node
-        cin = 3 #Assumes always a minimum sizes inverter. Could be specified in the tech.py file.
-        read_port_load = 0.5 #min size NMOS gate load
-        return logical_effort.logical_effort('bitline', size, cin, load+read_port_load, parasitic_delay, False)
+        size = 0.5 # This accounts for bitline being drained thought the access TX and internal node
+        cin = 3 # Assumes always a minimum sizes inverter. Could be specified in the tech.py file.
+        read_port_load = 0.5 # min size NMOS gate load
+        return logical_effort.logical_effort('bitline', size, cin, load + read_port_load, parasitic_delay, False)
 
     def input_load(self):
         """Return the relative capacitance of the access transistor gates"""
 
         # FIXME: This applies to bitline capacitances as well.
         # FIXME: sizing is not accurate with the handmade cell. Change once cell widths are fixed.
-        access_tx_cin = parameter["6T_access_size"]/drc["minwidth_tx"]
-        return 2*access_tx_cin
+        access_tx_cin = parameter["6T_access_size"] / drc["minwidth_tx"]
+        return 2 * access_tx_cin
 
     def build_graph(self, graph, inst_name, port_nets):
         """Adds edges to graph. Multiport bitcell timing graph is too complex
            to use the add_graph_edges function."""
-        debug.info(1,'Adding edges for {}'.format(inst_name))
-        pin_dict = {pin:port for pin,port in zip(self.pins, port_nets)}
+        debug.info(1, 'Adding edges for {}'.format(inst_name))
+        pin_dict = {pin: port for pin, port in zip(self.pins, port_nets)}
         pins = props.bitcell.cell_1w1r.pin
-        #Edges hardcoded here. Essentially wl->bl/br for the read port.
+        # Edges hardcoded here. Essentially wl->bl/br for the read port.
         # Port 1 edges
         graph.add_edge(pin_dict[pins.wl1], pin_dict[pins.bl1], self)
         graph.add_edge(pin_dict[pins.wl1], pin_dict[pins.br1], self)

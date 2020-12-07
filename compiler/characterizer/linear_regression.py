Fixed input scaling bugs delay prediction model

compiler/characterizer/linear_regression.py
@@ -22,17 +22,14 @@ class linear_regression():
     def __init__(self):
         self.model = None
 
-    def get_prediction(self, model_inputs):
-
-        train_sets = []
-        test_sets = []     
+    def get_prediction(self, model_inputs): 
                
         file_path = data_dir +'/'+data_filename
-        scaled_inputs = np.asarray(scale_input_datapoint(model_inputs, data_dir))
+        scaled_inputs = np.asarray([scale_input_datapoint(model_inputs, data_dir)])
 
         features, labels = get_scaled_data(file_path, data_dir)
         self.train_model(features, labels)
-        scaled_pred = model_prediction(model_inputs)
+        scaled_pred = self.model_prediction(scaled_inputs)
         pred = unscale_data(scaled_pred.tolist(), data_dir)
         debug.info(1,"Unscaled Prediction = {}".format(pred))
         return pred
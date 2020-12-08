971f2ac114a736b91f974c519b5bc78a47c31973
mrg <mrg@ucsc.edu>
2020-12-08 10:50:35 -0800

    v1.1.8

--- compiler/globals.py
+++ compiler/globals.py
@@ -19,7 +19,7 @@ import re
 import copy
 import importlib
 
-VERSION = "1.1.7"
+VERSION = "1.1.8"
 NAME = "OpenRAM v{}".format(VERSION)
 USAGE = "openram.py [options] <config file>\nUse -h for help.\n"
 

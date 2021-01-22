30fc81a1f02ad66cb5f551cbf8bb2cc31f388a05
Matt Guthaus <mrg@ucsc.edu>
2021-01-22 11:23:28 -0800

    Update copyright year.

--- compiler/tests/14_replica_pbitcell_array_test.py
+++ compiler/tests/14_replica_pbitcell_array_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # See LICENSE for licensing information.
 #
-# Copyright (c) 2016-2019 Regents of the University of California
+# Copyright (c) 2016-2021 Regents of the University of California
 # All rights reserved.
 #
 import unittest

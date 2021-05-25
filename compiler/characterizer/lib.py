76f5578cc1f2ec997dd5386f62bd9ab7bd89a5cf
Hunter Nichols <hznichol@ucsc.edu>
2021-05-25 15:19:27 -0700

    Removed path delays from characterization output to not disturb the current testing flow.

--- compiler/characterizer/lib.py
+++ compiler/characterizer/lib.py
@@ -645,12 +645,12 @@ class lib:
         if 'sim_time' in self.char_sram_results:
             self.pred_time = self.char_sram_results['sim_time']
         # Add to the OPTS to be written out as part of the extended OPTS file
-        # FIXME: should be written to datasheet, current version is simplifies current use of this    
-        if not self.use_model:
-            OPTS.sen_path_delays = self.char_sram_results["sen_path_measures"]
-            OPTS.sen_path_names = self.char_sram_results["sen_path_names"]
-            OPTS.bl_path_delays = self.char_sram_results["bl_path_measures"]
-            OPTS.bl_path_names = self.char_sram_results["bl_path_names"]
+        # FIXME: Temporarily removed from characterization output  
+        # if not self.use_model:
+            # OPTS.sen_path_delays = self.char_sram_results["sen_path_measures"]
+            # OPTS.sen_path_names = self.char_sram_results["sen_path_names"]
+            # OPTS.bl_path_delays = self.char_sram_results["bl_path_measures"]
+            # OPTS.bl_path_names = self.char_sram_results["bl_path_names"]
   
             
     def compute_setup_hold(self):

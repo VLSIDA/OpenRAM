013c5932a07782e7194bdec476cf4e41c14a294e
mrg <mrg@ucsc.edu>
2021-05-28 11:26:41 -0700

    Valid type is tree not single

--- compiler/router/supply_tree_router.py
+++ compiler/router/supply_tree_router.py
@@ -34,7 +34,7 @@ class supply_tree_router(router):
         # The pin escape router already made the bounding box big enough,
         # so we can use the regular bbox here.
         if pin_type:
-            debug.check(pin_type in ["left", "right", "top", "bottom", "single", "ring"],
+            debug.check(pin_type in ["left", "right", "top", "bottom", "tree", "ring"],
                         "Invalid pin type {}".format(pin_type))
         self.pin_type = pin_type
         router.__init__(self,

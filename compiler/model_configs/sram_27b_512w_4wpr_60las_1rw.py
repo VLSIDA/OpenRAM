84783bbac58f35a2298137d9a76ead1dc3e76b61
Hunter Nichols <hznichol@ucsc.edu>
2021-06-04 13:38:17 -0700

    Added more configs for model generation

---
+++ compiler/model_configs/sram_27b_512w_4wpr_60las_1rw.py
@@ -0,0 +1,9 @@
+word_size = 27
+num_words = 512
+words_per_row = 4
+local_array_size = 60
+
+output_extended_config = True
+output_datasheet_info = True
+netlist_only = True
+nominal_corner_only = True
---
+++ compiler/model_configs/sram_8b_256w_1wpr_1las_1rw.py
@@ -0,0 +1,9 @@
+word_size = 8
+num_words = 256
+words_per_row = 1
+local_array_size = 1
+
+output_extended_config = True
+output_datasheet_info = True
+netlist_only = True
+nominal_corner_only = True

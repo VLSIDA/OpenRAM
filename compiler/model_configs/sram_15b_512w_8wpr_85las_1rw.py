a73bfe6c2c10160fbd6fe26e97f1f8b4bcbbb954
Hunter Nichols <hznichol@ucsc.edu>
2021-06-09 10:35:58 -0700

    Added more configs for model and data from scn4m_subm run.

---
+++ compiler/model_configs/sram_15b_512w_8wpr_85las_1rw.py
@@ -0,0 +1,9 @@
+word_size = 15
+num_words = 512
+words_per_row = 8
+local_array_size = 85
+
+output_extended_config = True
+output_datasheet_info = True
+netlist_only = True
+nominal_corner_only = True

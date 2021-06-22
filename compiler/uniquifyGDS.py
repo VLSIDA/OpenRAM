b14992b213d61adc8602469d8da9fd099fc72338
mrg <mrg@ucsc.edu>
2021-06-22 16:18:03 -0700

    Fix arg off by one error in uniquifyGDS

--- compiler/uniquifyGDS.py
+++ compiler/uniquifyGDS.py
@@ -8,9 +8,7 @@ if len(sys.argv) < 3:
     print("Usage: {0} in.gds out.gds".format(sys.argv[0]))
     sys.exit(1)
 
-prefix = sys.argv[1]
-
-gds_file = sys.argv[2]
+gds_file = sys.argv[1]
 gds = gdsMill.VlsiLayout()
 reader = gdsMill.Gds2reader(gds)
 reader.loadFromFile(gds_file)
@@ -18,4 +16,4 @@ reader.loadFromFile(gds_file)
 gds.uniquify()
 
 writer = gdsMill.Gds2writer(gds)
-writer.writeToFile(sys.argv[3])
+writer.writeToFile(sys.argv[2])

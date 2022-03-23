9f7426052de24cef34ace79ddf8887f44cd8a4b4
mrg <mrg@ucsc.edu>
2022-03-23 14:46:41 -0700

    Split port_address tests

--- compiler/tests/18_port_address_16rows_test.py
+++ compiler/tests/18_port_address_16rows_test.py
@@ -14,24 +14,14 @@ from sram_factory import factory
 import debug
 
 
-class port_address_1rw_1r_test(openram_test):
+class port_address_test(openram_test):
 
     def runTest(self):
         config_file = "{}/tests/configs/config".format(os.getenv("OPENRAM_HOME"))
         globals.init_openram(config_file)
 
-        # Use the 2 port cell since it is usually bigger/easier
-        OPTS.num_rw_ports = 1
-        OPTS.num_r_ports = 1
-        OPTS.num_w_ports = 0
-        globals.setup_bitcell()
-
         debug.info(1, "Port address 16 rows")
         a = factory.create("port_address", cols=16, rows=16, port=0)
-        self.local_check(a)
-
-        debug.info(1, "Port address 256 rows")
-        a = factory.create("port_address", cols=256, rows=256, port=1)
         self.local_check(a)
 
         globals.end_openram()

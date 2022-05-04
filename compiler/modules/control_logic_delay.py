45239ca2a9715b4167184162ea321c7e9d998f26
samuelkcrow <samuelkcrow@gmail.com>
2022-05-04 12:51:31 -0700

    use cs_buf for sense amp on r ports instead of cs

--- compiler/modules/control_logic_delay.py
+++ compiler/modules/control_logic_delay.py
@@ -617,7 +617,7 @@ class control_logic_delay(design.design):
         if self.port_type=="rw":
             input_name = "we_bar"
         else:
-            input_name = "cs"
+            input_name = "cs_buf"
         # GATE FOR S_EN
         self.s_en_gate_inst = self.add_inst(name="and_s_en",
                                             mod=self.sen_and3)

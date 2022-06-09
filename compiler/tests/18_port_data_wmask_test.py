9e3a28237f75d55401f1920cd84823d9bb302347
mrg <mrg@ucsc.edu>
2022-06-08 17:18:53 -0700

    Update port data test for sky130 single port

--- compiler/tests/18_port_data_wmask_test.py
+++ compiler/tests/18_port_data_wmask_test.py
@@ -22,9 +22,18 @@ class port_data_wmask_test(openram_test):
         globals.init_openram(config_file)
         from sram_config import sram_config
 
+        if OPTS.tech_name == "sky130":
+            num_spare_rows = 1
+            num_spare_cols = 1
+        else:
+            num_spare_rows = 0
+            num_spare_cols = 0
+
         c = sram_config(word_size=16,
                         write_size=4,
-                        num_words=16)
+                        num_words=16,
+                        num_spare_cols=num_spare_cols,
+                        num_spare_rows=num_spare_rows)
 
         c.words_per_row = 1
         factory.reset()

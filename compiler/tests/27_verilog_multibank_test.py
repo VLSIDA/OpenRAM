6a4bd622067d25ddae5e6c695c96380f0ecfbe09
Bugra Onal <onalbugra@gmail.com>
2022-07-13 16:40:21 -0700

    Added unit test for multibank

---
+++ compiler/tests/27_verilog_multibank_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# See LICENSE for licensing information.
+#
+# Copyright (c) 2016-2021 Regents of the University of California and The Board
+# of Regents for the Oklahoma Agricultural and Mechanical College
+# (acting for and on behalf of Oklahoma State University)
+# All rights reserved.
+#
+import unittest
+from testutils import *
+import sys, os
+sys.path.append(os.getenv("OPENRAM_HOME"))
+import globals
+from globals import OPTS
+import debug
+
+
+class multibank_verilog_test(openram_test):
+
+    def runTest(self):
+        config_file = "{}/tests/configs/config".format(os.getenv("OPENRAM_HOME"))
+        globals.init_openram(config_file)
+        OPTS.route_supplies=False
+        OPTS.check_lvsdrc=False
+        OPTS.netlist_only=True
+        from sram import sram
+        from sram_config import sram_config
+        c = sram_config(word_size=2,
+                        num_words=16,
+                        num_banks=2)
+        c.words_per_row=1
+        c.recompute_sizes()
+        debug.info(1, "Testing Verilog for sample 2 bit, 16 words SRAM with 2 bank")
+        # This doesn't have to use the factory since worst case
+        # it will just replaece the top-level module of the same name
+        s = sram(c, "sram_2_16_2_{0}".format(OPTS.tech_name))
+
+        vfile = s.name + ".v"
+        vname = OPTS.openram_temp + vfile
+        s.verilog_write(vname)
+
+
+        # let's diff the result with a golden model
+        golden = "{0}/golden/{1}".format(os.path.dirname(os.path.realpath(__file__)), vfile)
+        self.assertTrue(self.isdiff(vname, golden))
+
+        globals.end_openram()
+
+# run the test from the command line
+if __name__ == "__main__":
+    (OPTS, args) = globals.parse_args()
+    del sys.argv[1:]
+    header(__file__, OPTS.tech_name)
+    unittest.main(testRunner=debugTestRunner())

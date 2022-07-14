c406e2a9dac05752a6ee4ec7692adb8e7ac612d0
mrg <mrg@ucsc.edu>
2022-07-13 17:19:25 -0700

    Make macros use same DOCKER_CMD.

--- openram.mk
+++ openram.mk
@@ -17,6 +17,7 @@ export PDK_ROOT
 UID = $(shell id -u)
 GID = $(shell id -g)
 
+export OPENRAM_TMP=$(OPENRAM_DIR)/results/$*/tmp 
 export DOCKER_CMD= docker run \
 		-v $(TOP_DIR):/openram \
 		-v $(FREEPDK45):/freepdk45 \
@@ -26,11 +27,11 @@ export DOCKER_CMD= docker run \
 		-e PDKPATH=/pdk/sky130A \
         -e OPENRAM_HOME=/openram/compiler \
         -e OPENRAM_TECH=/openram/technology \
-		-e OPENRAM_TMP=$(OPENRAM_DIR)/results/$*/tmp \
+		-e OPENRAM_TMP=$(OPENRAM_TMP)\
         -e PYTHONPATH=/openram/compiler \
 		-v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro \
 		--user $(UID):$(GID) \
-        	vlsida/openram-ubuntu:latest
+        vlsida/openram-ubuntu:latest
 
 mount:
 	@docker run -it \

d8e0f4275d566bf12dc975ce72ab5d9d21e113de
samuelkcrow <samuelkcrow@gmail.com>
2022-12-14 14:20:54 -0800

    fix capped array after merge with dev

--- compiler/modules/capped_bitcell_array.py
+++ compiler/modules/capped_bitcell_array.py
@@ -4,14 +4,14 @@
 # All rights reserved.
 #
 
-import debug
-from base import vector
-from base import contact
+from openram import debug
+from openram.base import vector
+from openram.base import contact
+from openram.sram_factory import factory
+from openram.tech import drc, spice
+from openram.tech import cell_properties as props
+from openram import OPTS
 from .bitcell_base_array import bitcell_base_array
-from tech import drc, spice
-from tech import cell_properties as props
-from globals import OPTS
-from sram_factory import factory
 
 
 class capped_bitcell_array(bitcell_base_array):

1b711ed7d724c86c41e51f7a342533be3eaa8840
mrg <mrg@ucsc.edu>
2023-02-17 15:37:53 -0800

    Include and use FreePDK45 models with license.

--- technology/freepdk45/__init__.py
+++ technology/freepdk45/__init__.py
@@ -18,16 +18,16 @@ from openram import debug
 TECHNOLOGY = "freepdk45"
 
 ##########################
-# FreePDK45 paths
-
-PDK_PATH=os.environ.get("FREEPDK45")
-if PDK_PATH==None:
-    debug.error("Must define FREEPDK45 to point to PDK.", -1)
-PDK_DIR=os.path.abspath(os.path.expanduser(PDK_PATH))
-os.environ["PDK_DIR"] = PDK_DIR
-os.environ["SYSTEM_CDS_LIB_DIR"] = "{0}/ncsu_basekit/cdssetup".format(PDK_DIR)
-os.environ["CDS_SITE"] = PDK_DIR
-os.environ["MGC_TMPDIR"] = "/tmp"
+# Cadence FreePDK45 paths
+#PDK_PATH=os.environ.get("FREEPDK45")
+#if PDK_PATH==None:
+#    debug.error("Must define FREEPDK45 to point to PDK.", -1)
+#PDK_DIR=os.path.abspath(os.path.expanduser(PDK_PATH))
+#os.environ["PDK_DIR"] = PDK_DIR
+#os.environ["SYSTEM_CDS_LIB_DIR"] = "{0}/ncsu_basekit/cdssetup".format(PDK_DIR)
+#os.environ["CDS_SITE"] = PDK_DIR
+#os.environ["MGC_TMPDIR"] = "/tmp"
+#os.environ["SYSTEM_CDS_LIB_DIR"] = "{0}/ncsu_basekit/cdssetup".format(PDK_DIR)
 
 ###########################
 #OpenRAM Paths
@@ -35,11 +35,15 @@ os.environ["MGC_TMPDIR"] = "/tmp"
 try:
     DRCLVS_HOME = os.path.abspath(os.environ.get("DRCLVS_HOME"))
 except:
-    DRCLVS_HOME= PDK_DIR+"/ncsu_basekit/techfile/calibre"
+    tech_paths = os.environ.get("OPENRAM_TECH").split(':')
+    for d in tech_paths:
+        if os.path.isdir(d+"/freepdk45"):
+            DRCLVS_HOME= d+"/freepdk45/tech"
+            break
+
+# If you are using Cadence, you should set the DRCLVS_HOME environment variable
+# to the FreePDK45 PDK location:
+# DRCLVS_HOME= PDK_DIR+"/ncsu_basekit/techfile/calibre"
 os.environ["DRCLVS_HOME"] = DRCLVS_HOME
 
-# try:
-#      SPICE_MODEL_DIR = os.path.abspath(os.environ.get("SPICE_MODEL_DIR"))
-# except:
-# Always use the one in the PDK dir for FreePDK45
-os.environ["SPICE_MODEL_DIR"] = PDK_DIR+"/ncsu_basekit/models/hspice/tran_models"
+os.environ["SPICE_MODEL_DIR"] = "{0}/models/hspice/tran_models".format(os.path.dirname(__file__))

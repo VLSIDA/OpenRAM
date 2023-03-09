577cdcb232f7a302ce67e1ad2645ed7bd4d8837e
Jacob Walker <jswalker@ucsc.edu>
2023-03-09 12:58:04 -0800

    example configs

---
+++ macros/configs/example_config_rom.py
@@ -0,0 +1,17 @@
+# See LICENSE for licensing information.
+#
+# Copyright (c) 2016-2023 Regents of the University of California and The Board
+# of Regents for the Oklahoma Agricultural and Mechanical College
+# (acting for and on behalf of Oklahoma State University)
+# All rights reserved.
+#
+
+word_size = 1
+
+nominal_corner_only = True
+
+output_name = "rom"
+
+spice_name = "ngspice"
+
+rom_data = "/home/jswalker/PrivateRAM/macros/rom_example_data/rom_data_1kB"

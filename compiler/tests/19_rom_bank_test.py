7c453e80be5e73ee55a700114fc6837c298e0e50
mrg <mrg@ucsc.edu>
2023-03-13 16:42:24 -0700

    Simplify ROM test.

--- compiler/tests/19_rom_bank_test.py
+++ compiler/tests/19_rom_bank_test.py
@@ -21,15 +21,14 @@ class rom_bank_test(openram_test):
     def runTest(self):
         config_file = "{}/tests/configs/config".format(os.getenv("OPENRAM_HOME"))
         openram.init_openram(config_file, is_unit_test=True)
-        debug.info(1, "Testing 4kB rom cell")
-
-        test_data = "{0}/{1}/rom_data_4kB".format(os.getenv("OPENRAM_HOME"), OPTS.rom_data_dir)
+        debug.info(1, "Testing rom cell")
+        test_data = "{0}/{1}/rom_data_64B".format(os.getenv("OPENRAM_HOME"), OPTS.rom_data_dir)
 
         from openram import rom_config
 
         conf = rom_config(strap_spacing = 8,
                           rom_data = test_data,
-                          word_size = 2)
+                          word_size = 1)
 
         a = factory.create(module_type="rom_bank", rom_config=conf)
         a.sp_write(OPTS.openram_temp + 'simulation_file.sp')

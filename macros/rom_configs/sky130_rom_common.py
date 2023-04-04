7ed7ff4e9a61a20e67eb61641d191ef8be897f33
Sage Walker <cuby474@gmail.com>
2023-04-03 17:06:09 -0700

    fixed bug with tempdir in macros makefile

--- macros/rom_configs/sky130_rom_common.py
+++ macros/rom_configs/sky130_rom_common.py
@@ -1,4 +1,10 @@
-
+# See LICENSE for licensing information.
+#
+# Copyright (c) 2016-2023 Regents of the University of California and The Board
+# of Regents for the Oklahoma Agricultural and Mechanical College
+# (acting for and on behalf of Oklahoma State University)
+# All rights reserved.
+#
 tech_name = "sky130"
 nominal_corner_only = True
 

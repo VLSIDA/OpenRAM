42257fb7f8cc56502d83a4ef99afedc24b7717d0
Eren Dogan <erdogan@ucsc.edu>
2023-08-01 11:25:12 -0700

    Export router_tech again

--- compiler/router/__init__.py
+++ compiler/router/__init__.py
@@ -3,5 +3,6 @@
 # Copyright (c) 2016-2023 Regents of the University of California, Santa Cruz
 # All rights reserved.
 #
+from .router_tech import *
 from .signal_escape_router import *
 from .supply_router import *

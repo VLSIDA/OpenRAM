50772821808818ca12ff5d4d3a75d51bf420b37d
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2026-04-28 14:04:42 -0700

    count wordlines from bottom going up

--- compiler/modules/bitcell_base_array.py
+++ compiler/modules/bitcell_base_array.py
@@ -151,7 +151,7 @@ class bitcell_base_array(design):
         wl_names = self.cell.get_all_wl_names()
         for row in range(self.row_size):
             for port in self.all_ports:
-                wl_pin = self.cell_inst[self.row_size - 1 - row, 0].get_pin(wl_names[port])
+                wl_pin = self.cell_inst[row, 0].get_pin(wl_names[port])
                 self.add_layout_pin(text="wl_{0}_{1}".format(port, row),
                                     layer=wl_pin.layer,
                                     offset=wl_pin.ll().scale(0, 1),
